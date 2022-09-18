8a9e18e673be3b882f3f39ca0e0f0541e51ce6a6
Patryk Lizoń <lizon.patryk@gmail.com>
2022-09-18 22:52:06 +0200

    feat(easy): #05 Add solution for nonConstructibleChange

---
+++ src/easy/05-non-constructible-change.spec.ts
@@ -0,0 +1,29 @@
+import { test } from "uvu";
+import assert from "uvu/assert";
+
+import { type TestCases } from "../utils/types";
+
+import { nonConstructibleChange } from "./05-non-constructible-change";
+
+test("returns the name of the team with the highest score", () => {
+  const testCases: TestCases<typeof nonConstructibleChange> = [
+    [[[]], 1],
+    [[[1]], 2],
+    [[[2]], 1],
+    [[[1, 7]], 2],
+    [[[1, 2, 5]], 4],
+    [[[1, 1, 1, 1, 1]], 6],
+    [[[1, 1, 2, 3, 5, 7, 22]], 20],
+  ];
+
+  testCases.forEach(([args, expected], idx) => {
+    assert.equal(
+      nonConstructibleChange(...args),
+      expected,
+      `failed test case at idx: ${idx} for args:
+     > [${args[0].join(", ")}]`
+    );
+  });
+});
+
+test.run();
