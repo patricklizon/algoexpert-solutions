eeeacede2695da42c60d477b65552589f98efd3b
Patryk Lizoń <lizon.patryk@gmail.com>
2022-09-20 01:38:37 +0200

    feat(easy): #08 Add solution for nodeDepths

---
+++ src/easy/08-node-depth.spec.ts
@@ -0,0 +1,98 @@
+import { test } from "uvu";
+import assert from "uvu/assert";
+
+import type { TestCases } from "../utils";
+import { makeTree } from "../utils/tree";
+
+import { nodeDepths } from "./08-node-depth";
+
+test("sums depths of binary tree's nodes", () => {
+  const testCases: TestCases<typeof nodeDepths> = [
+    [
+      [
+        makeTree({
+          nodes: [
+            { id: "1", left: "2", right: "3", value: 1 },
+            { id: "2", left: "4", right: "5", value: 2 },
+            { id: "3", left: "6", right: "7", value: 3 },
+            { id: "4", left: "8", right: "9", value: 4 },
+            { id: "5", left: null, right: null, value: 5 },
+            { id: "6", left: null, right: null, value: 6 },
+            { id: "7", left: null, right: null, value: 7 },
+            { id: "8", left: null, right: null, value: 8 },
+            { id: "9", left: null, right: null, value: 9 },
+          ],
+          root: "1",
+        }),
+      ],
+      16,
+    ],
+    [
+      [
+        makeTree({
+          nodes: [{ id: "1", left: null, right: null, value: 1 }],
+          root: "1",
+        }),
+      ],
+      0,
+    ],
+    [
+      [
+        makeTree({
+          nodes: [
+            { id: "1", left: "2", right: null, value: 1 },
+            { id: "2", left: null, right: null, value: 2 },
+          ],
+          root: "1",
+        }),
+      ],
+      1,
+    ],
+    [
+      [
+        makeTree({
+          nodes: [
+            { id: "1", left: "2", right: "3", value: 1 },
+            { id: "2", left: null, right: null, value: 2 },
+            { id: "3", left: null, right: null, value: 3 },
+          ],
+          root: "1",
+        }),
+      ],
+      2,
+    ],
+    [
+      [
+        makeTree({
+          nodes: [
+            { id: "1", left: "2", right: "8", value: 1 },
+            { id: "2", left: "3", right: null, value: 2 },
+            { id: "3", left: "4", right: null, value: 3 },
+            { id: "4", left: "5", right: null, value: 4 },
+            { id: "5", left: "6", right: null, value: 5 },
+            { id: "6", left: null, right: "7", value: 6 },
+            { id: "7", left: null, right: null, value: 7 },
+            { id: "8", left: null, right: "9", value: 8 },
+            { id: "9", left: null, right: "10", value: 9 },
+            { id: "10", left: null, right: "11", value: 10 },
+            { id: "11", left: null, right: "12", value: 11 },
+            { id: "12", left: "13", right: null, value: 12 },
+            { id: "13", left: null, right: null, value: 13 },
+          ],
+          root: "1",
+        }),
+      ],
+      42,
+    ],
+  ];
+
+  testCases.forEach(([args, expected], idx) => {
+    assert.equal(
+      nodeDepths(...args),
+      expected,
+      `failed test case at idx: ${idx}`
+    );
+  });
+});
+
+test.run();
---
+++ src/easy/08-node-depth.ts
@@ -0,0 +1,58 @@
+/**
+
+
+  The distance between a node in a Binary Tree and the tree's root is called the
+  node's depth.
+
+
+  Write a function that takes in a Binary Tree and returns the sum of its nodes
+  depths.
+
+
+  Each BinaryTree node has an integer value, a
+  left child node, and a right child node. Children
+  nodes can either be BinaryTree nodes themselves or
+  None / null.
+
+  Sample Input
+  tree =    1
+        /     \
+        2       3
+      /   \   /   \
+    4     5 6     7
+  /   \
+  8     9
+
+  Sample Output
+  16
+  // The depth of the node with value 2 is 1.
+  // The depth of the node with value 3 is 1.
+  // The depth of the node with value 4 is 2.
+  // The depth of the node with value 5 is 2.
+  // Etc..
+  // Summing all of these depths yields 16.
+
+
+ *
+ */
+
+import { BinaryTree } from "../utils/tree";
+
+export function nodeDepths(root: BinaryTree): number {
+  const stack: { node: BinaryTree; depth: number }[] = [];
+  let sum = 0;
+
+  stack.push({ node: root, depth: 0 });
+
+  while (stack.length > 0) {
+    const s = stack.pop();
+    if (!s) throw new Error("never");
+
+    sum += s.depth;
+
+    if (s.node.left) stack.push({ node: s.node.left, depth: s.depth + 1 });
+    if (s.node.right) stack.push({ node: s.node.right, depth: s.depth + 1 });
+  }
+
+  return sum;
+}
