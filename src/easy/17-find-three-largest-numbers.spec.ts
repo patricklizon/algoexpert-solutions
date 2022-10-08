e1ad3f395dec3b1d50cfb29c1cf59cdb670d3b26
Patryk Lizoń <lizon.patryk@gmail.com>
2022-10-08 19:06:48 +0200

    fix: test message

--- src/easy/17-find-three-largest-numbers.spec.ts
+++ src/easy/17-find-three-largest-numbers.spec.ts
@@ -5,7 +5,7 @@ import type { TestCases } from "../utils";
 
 import { findThreeLargestNumbers } from "./17-find-three-largest-numbers";
 
-test("finds number using binary search algorithm", () => {
+test("finds three largest numbers", () => {
   const testCases: TestCases<typeof findThreeLargestNumbers> = [
     [[[10, 5, 9, 10, 12]], [10, 10, 12]],
     [[[141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]], [18, 141, 541]],
