b8aed86390b723f77a04c9c985d300d05cab9284
Patryk Lizoń <lizon.patryk@gmail.com>
2022-12-30 17:52:12 +0100

    fix: test message

--- src/medium/07-array-of-products.spec.ts
+++ src/medium/07-array-of-products.spec.ts
@@ -2,7 +2,7 @@ import { runTestCases, TestCases } from "../utils";
 
 import { arrayOfProducts as fn } from "./07-array-of-products";
 
-test("finds the highest peak", () => {
+test("returns array of duplicates", () => {
   const testCases: TestCases<typeof fn> = [
     [[[]], []],
     [[[5, 1, 4, 2]], [8, 40, 10, 20]],
